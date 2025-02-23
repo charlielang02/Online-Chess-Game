56d088d9bb86ad8f019773fea8a4ab3b058a4920
Charlie Lang <113050599+charlielang02@users.noreply.github.com>
2025-02-22 18:21:26 -0700

    remove magic numbers

--- piece.py
+++ piece.py
@@ -1,6 +1,14 @@
 import pygame
 import os
 
+PIECE_SIZE = (55, 55)
+BOARD_SIZE = 8
+BOARD_MAX_INDEX = BOARD_SIZE - 1
+X_COORDINATE = 113
+Y_COORDINATE = 113
+BOARD_WIDTH = 525
+BOARD_HEIGHT = 525
+
 b_bishop = pygame.image.load(os.path.join("img", "black_bishop.png"))
 b_king = pygame.image.load(os.path.join("img", "black_king.png"))
 b_knight = pygame.image.load(os.path.join("img", "black_knight.png"))
@@ -22,15 +30,15 @@ B = []
 W = []
 
 for img in b:
-    B.append(pygame.transform.scale(img, (55, 55)))
+    B.append(pygame.transform.scale(img, PIECE_SIZE))
 
 for img in w:
-    W.append(pygame.transform.scale(img, (55, 55)))
+    W.append(pygame.transform.scale(img, PIECE_SIZE))
 
 
 class Piece:
     img = -1
-    rect = (113, 113, 525, 525)
+    (X_COORDINATE,Y_COORDINATE,BOARD_WIDTH,BOARD_HEIGHT)
     startX = rect[0]
     startY = rect[1]
 
@@ -55,8 +63,8 @@ class Piece:
         else:
             drawThis = B[self.img]
 
-        x = (4 - self.col) + round(self.startX + (self.col * self.rect[2] / 8))
-        y = 3 + round(self.startY + (self.row * self.rect[3] / 8))
+        x = (4 - self.col) + round(self.startX + (self.col * self.rect[2] / BOARD_SIZE))
+        y = 3 + round(self.startY + (self.row * self.rect[3] / BOARD_SIZE))
 
         if self.selected and self.color == color:
             pygame.draw.rect(win, (255, 0, 0), (x, y, 62, 62), 4)
@@ -84,7 +92,7 @@ class Bishop(Piece):
         djL = j + 1
         djR = j - 1
         for di in range(i - 1, -1, -1):
-            if djL < 8:
+            if djL < BOARD_SIZE:
                 p = board[di][djL]
                 if p == 0:
                     moves.append((djL, di))
@@ -116,8 +124,8 @@ class Bishop(Piece):
         # TOP LEFT
         djL = j + 1
         djR = j - 1
-        for di in range(i + 1, 8):
-            if djL < 8:
+        for di in range(i + 1, BOARD_SIZE):
+            if djL < BOARD_SIZE:
                 p = board[di][djL]
                 if p == 0:
                     moves.append((djL, di))
@@ -129,7 +137,7 @@ class Bishop(Piece):
             else:
                 break
             djL += 1
-        for di in range(i + 1, 8):
+        for di in range(i + 1, BOARD_SIZE):
             if djR > -1:
                 p = board[di][djR]
                 if p == 0:
@@ -177,14 +185,14 @@ class King(Piece):
                 moves.append((j, i - 1))
 
             # TOP RIGHT
-            if j < 7:
+            if j < BOARD_MAX_INDEX:
                 p = board[i - 1][j + 1]
                 if p == 0:
                     moves.append((j + 1, i - 1,))
                 elif p.color != self.color:
                     moves.append((j + 1, i - 1,))
 
-        if i < 7:
+        if i < BOARD_MAX_INDEX:
             # BOTTOM LEFT
             if j > 0:
                 p = board[i + 1][j - 1]
@@ -201,7 +209,7 @@ class King(Piece):
                 moves.append((j, i + 1))
 
             # BOTTOM RIGHT
-            if j < 7:
+            if j < BOARD_MAX_INDEX:
                 p = board[i + 1][j + 1]
                 if p == 0:
                     moves.append((j + 1, i + 1))
@@ -217,7 +225,7 @@ class King(Piece):
                 moves.append((j - 1, i))
 
         # MIDDLE RIGHT
-        if j < 7:
+        if j < BOARD_MAX_INDEX:
             p = board[i][j + 1]
             if p == 0:
                 moves.append((j + 1, i))
@@ -253,7 +261,7 @@ class Knight(Piece):
                 moves.append((j - 1, i - 2))
 
         # DOWN RIGHT
-        if i < 6 and j < 7:
+        if i < 6 and j < BOARD_MAX_INDEX:
             p = board[i + 2][j + 1]
             if p == 0:
                 moves.append((j + 1, i + 2))
@@ -261,7 +269,7 @@ class Knight(Piece):
                 moves.append((j + 1, i + 2))
 
         # UP RIGHT
-        if i > 1 and j < 7:
+        if i > 1 and j < BOARD_MAX_INDEX:
             p = board[i - 2][j + 1]
             if p == 0:
                 moves.append((j + 1, i - 2))
@@ -282,14 +290,14 @@ class Knight(Piece):
             elif p.color != self.color:
                 moves.append((j + 2, i - 1))
 
-        if i < 7 and j > 1:
+        if i < BOARD_MAX_INDEX and j > 1:
             p = board[i + 1][j - 2]
             if p == 0:
                 moves.append((j - 2, i + 1))
             elif p.color != self.color:
                 moves.append((j - 2, i + 1))
 
-        if i < 7 and j < 6:
+        if i < BOARD_MAX_INDEX and j < 6:
             p = board[i + 1][j + 2]
             if p == 0:
                 moves.append((j + 2, i + 1))
@@ -315,13 +323,13 @@ class Pawn(Piece):
         moves = []
         try:
             if self.color == "b":
-                if i < 7:
+                if i < BOARD_MAX_INDEX:
                     p = board[i + 1][j]
                     if p == 0:
                         moves.append((j, i + 1))
 
                     # DIAGONAL
-                    if j < 7:
+                    if j < BOARD_MAX_INDEX:
                         p = board[i + 1][j + 1]
                         if p != 0:
                             if p.color != self.color:
@@ -349,7 +357,7 @@ class Pawn(Piece):
                     if p == 0:
                         moves.append((j, i - 1))
 
-                if j < 7:
+                if j < BOARD_MAX_INDEX:
                     p = board[i - 1][j + 1]
                     if p != 0:
                         if p.color != self.color:
@@ -388,7 +396,7 @@ class Queen(Piece):
         djL = j + 1
         djR = j - 1
         for di in range(i - 1, -1, -1):
-            if djL < 8:
+            if djL < BOARD_SIZE:
                 p = board[di][djL]
                 if p == 0:
                     moves.append((djL, di))
@@ -416,8 +424,8 @@ class Queen(Piece):
         # TOP LEFT
         djL = j + 1
         djR = j - 1
-        for di in range(i + 1, 8):
-            if djL < 8:
+        for di in range(i + 1, BOARD_SIZE):
+            if djL < BOARD_SIZE:
                 p = board[di][djL]
                 if p == 0:
                     moves.append((djL, di))
@@ -427,7 +435,7 @@ class Queen(Piece):
                 else:
                     djL = 9
             djL += 1
-        for di in range(i + 1, 8):
+        for di in range(i + 1, BOARD_SIZE):
             if djR > -1:
                 p = board[di][djR]
                 if p == 0:
@@ -452,7 +460,7 @@ class Queen(Piece):
                 break
 
         # DOWN
-        for x in range(i + 1, 8, 1):
+        for x in range(i + 1, BOARD_SIZE, 1):
             p = board[x][j]
             if p == 0:
                 moves.append((j, x))
@@ -474,7 +482,7 @@ class Queen(Piece):
                 break
 
         # RIGHT
-        for x in range(j + 1, 8, 1):
+        for x in range(j + 1, BOARD_SIZE, 1):
             p = board[i][x]
             if p == 0:
                 moves.append((x, i))
@@ -508,7 +516,7 @@ class Rook(Piece):
                 break
 
         # DOWN
-        for x in range(i + 1, 8, 1):
+        for x in range(i + 1, BOARD_SIZE, 1):
             p = board[x][j]
             if p == 0:
                 moves.append((j, x))
@@ -530,7 +538,7 @@ class Rook(Piece):
                 break
 
         # RIGHT
-        for x in range(j + 1, 8, 1):
+        for x in range(j + 1, BOARD_SIZE, 1):
             p = board[i][x]
             if p == 0:
                 moves.append((x, i))
